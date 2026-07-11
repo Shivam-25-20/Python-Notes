8501883e0faa3bead54083cb17f1847bc0a7b822
Shivam Vinay Singh <svsingh252004@gmail.com>
2026-07-11 21:15:25 +0530

    Add Python Part 2

---
+++ Python Part 2/conversion.py
@@ -0,0 +1,23 @@
+x = "100"
+
+print(int(x))
+print(type(int(x)))
+x = 1000.233
+
+print(int(x))
+print(type(int(x)))
+x = True
+
+print(int(x))
+print(type(int(x)))
+
+x = "100.788"
+
+print(float(x))
+print(type(float(x)))
+x = 1000
+
+print(float(x))
+print(type(float(x)))
+print(bool("1"))
+print(bool(0))
